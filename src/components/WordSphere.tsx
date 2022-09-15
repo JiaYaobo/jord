f16a84a6769d1f1fb5ba362c7c60164cee9bef6d
jiayaobo <januzaj2011@163.com>
2022-09-15 23:10:19 +0800

    update deps

--- src/components/WordSphere.tsx
+++ src/components/WordSphere.tsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import WordModel from './WordModel'
+import { Canvas } from '@react-three/fiber'
+import { Environment, OrbitControls } from '@react-three/drei'
+import styled from "@emotion/styled"
+import { motion, MotionCanvas, LayoutCamera } from "framer-motion-3d"
+
+
+const Container = styled(motion.div)`
+
+  background-color: #ee1f1f;  
+  width: 50vw;
+  height: 50vh;
+`
 
 
 const WordSphere = () => {
   return (
-    <div>
+    <Container>
         <WordModel/>
-    </div>
+    </Container>
   )
 }
 
